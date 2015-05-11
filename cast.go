43e67959a904e47b16d4e4bdfad77686f1e8404b
Peter Bakkum <pbb7c@virginia.edu>
2015-05-10 23:00:32 -0700

    Added config file parsing

--- cast.go
+++ cast.go
@@ -1,13 +1,18 @@
 package main
 
 import "fmt"
-import "os/exec"
 import "os"
+import "os/exec"
+import "os/user"
 import "bufio"
 import "sync"
+import "io/ioutil"
+import "gopkg.in/yaml.v2"
 
 const color_red = "\033[0;31m"
 const color_clear = "\033[0m"
+
+type Hash map[interface{}]interface{}
 
 func printOut(prefix string, x string) {
   fmt.Printf("[%s] %s\n", prefix, x)
@@ -48,6 +53,7 @@ func ssh(target string, command string, globalWaitGroup *sync.WaitGroup) {
   var wg sync.WaitGroup
   wg.Add(2)
 
+  // asynchronous scan/print loop for stdout
   go func(out bufio.Scanner) {
     for(out.Scan()) {
       printOut(target, out.Text())
@@ -55,6 +61,7 @@ func ssh(target string, command string, globalWaitGroup *sync.WaitGroup) {
     wg.Done()
   }(*outScanner)
 
+  // asynchronous scan/print loop for stderr
   go func(err bufio.Scanner) {
     for (err.Scan()) {
       printErrString(target, err.Text())
@@ -72,11 +79,60 @@ func ssh(target string, command string, globalWaitGroup *sync.WaitGroup) {
   globalWaitGroup.Done()
 }
 
+func getDefaultConfigFilename() string {
+  usr, err := user.Current()
+  if err != nil {
+    printErr("cast", err)
+    os.Exit(3)
+  }
+  return usr.HomeDir + "/.cast.yml"
+}
+
+func readConfig(filename string) Hash {
+  content, err := ioutil.ReadFile(filename)
+
+  if err != nil {
+    printErr("cast", err)
+    os.Exit(2)
+  }
+
+  m := make(Hash)
+
+  err = yaml.Unmarshal(content, &m)
+  if err != nil {
+    printErr("cast", err)
+    os.Exit(2)
+  }
+
+  return m
+}
+
+func getTargetHosts(targets []string, config Hash) []string {
+  x := make([]string, 32)
+
+  for _, name := range targets {
+    group := config[name]
+
+    if group == nil {
+      x = append(x, name)
+    } else {
+      for _, hostname := range group.([]interface{}) {
+        x = append(x, hostname.(string))
+      }
+    }
+  }
+
+  return x
+}
+
 func main() {
+  config := readConfig(getDefaultConfigFilename())
+  targets := []string{"group1"}
+  targets = getTargetHosts(targets, config)
+
   var waitGroup sync.WaitGroup
   waitGroup.Add(1)
   go ssh("oxblood3", "ls -la /", &waitGroup)
   waitGroup.Wait()
 }
-
 
